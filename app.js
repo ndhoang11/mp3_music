0b9d9bd88e625c979598cee7a74c19f91b6f72c9
ndhoang11 <nguyenduyhoangqx@gmail.com>
2023-05-09 21:18:44 +0700

    update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 const path = require("path");
 
-app.use('/assets',express.static('assets'))
+app.use('/assets',express.static(path.join(__dirname, 'assets')))
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
